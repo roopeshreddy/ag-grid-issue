74be703bcec04a184945179ec858b79bc3bd9621
Roopesh Chinnakampalli <rchinnakampalli@worldbankgroup.org>
2020-06-18 14:25:52 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GridApi, GridReadyEvent, ExcelExportParams } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agGrid';
+  gridApi: GridApi;
+
+  columnDefs = [
+    {field: 'make', cellRenderer: (params) => { return `<a href="javascript:;">${params.value}</a>` } },
+    {field: 'model' },
+    {field: 'price', cellStyle: { 'text-transform': 'none' }}
+];
+
+rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+];
+
+onGridReady($event: GridReadyEvent) {
+this.gridApi = $event.api;
+}
+
+exportToExcel() {
+  const params: ExcelExportParams = {
+    fileName: 'test',
+    exportMode: 'xlsx'
+  };
+  this.gridApi.exportDataAsExcel(params);
+}
+
 }
